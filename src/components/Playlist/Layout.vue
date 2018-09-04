59f25c8165f929f450c715e9f9ccda43f17b6431
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2018-09-04 22:47:51 +0800

    修改 右侧属性框

--- src/components/Playlist/Layout.vue
+++ src/components/Playlist/Layout.vue
@@ -89,7 +89,7 @@
           <el-aside class="layout-main-right">
             <!--组件属性-->
             <div class="layout-assembly-attribute">
-              <div>
+              <div class="attribute-group">
                 <label>对齐</label>
                 <span>
                   <i class="icon iconfont icon-align-left"></i>
@@ -97,14 +97,101 @@
                   <i class="icon iconfont icon-align-right"></i>
                 </span>
               </div>
-              <div>
+              <div class="attribute-group">
                 <label>位置</label>
                 <span>
-                  X <el-input v-model="selectElement.style.left" size="mini"></el-input>
+                  X&nbsp;&nbsp;<el-input v-model="selectElement.style.left" size="mini" class="attribute-position-input"></el-input>
+                </span>
+                <span>
+                  Y&nbsp;&nbsp;<el-input v-model="selectElement.style.top" size="mini" class="attribute-position-input"></el-input>
+                </span>
+              </div>
+              <div class="attribute-group">
+                <label>大小</label>
+                <span>
+                  宽 <el-input v-model="selectElement.style.width" size="mini" class="attribute-position-input"></el-input>
+                </span>
+                <span>
+                  高 <el-input v-model="selectElement.style.height" size="mini" class="attribute-position-input"></el-input>
+                </span>
+              </div>
+              <div class="attribute-group">
+                <el-button>上移一层</el-button>
+                <el-button>下移一层</el-button>
+              </div>
+            </div>
+            <!--组件参数-->
+            <!--字幕组建参数-->
+            <div class="layout-assembly-attribute" v-if="selectElement.type === 1">
+              <div class="attribute-group">
+                <label>方向</label>
+                <span>
+                  <el-radio v-model="selectElement.parames.direction" label="1">从右到左</el-radio>
+                  <el-radio v-model="selectElement.parames.direction" label="2">从下到上</el-radio>
+                </span>
+              </div>
+              <div class="attribute-group">
+                <label>速度</label>
+                <span>
+                  <el-input-number v-model="selectElement.parames.speed" controls-position="right" :min="0" :max="10" size="mini"></el-input-number>
+                </span>
+              </div>
+              <div class="attribute-group">
+                <label>字体</label>
+                <span>
+                  <el-select class="attribute-position-select" v-model="selectElement.parames.fontFamily" placeholder="请选择" size="mini">
+                    <el-option label="默认字体" value="1"></el-option>
+                  </el-select>
+                </span>
+              </div>
+              <div class="attribute-group">
+                <label>背景颜色</label>
+                <span>
+                  <el-color-picker v-model="selectElement.parames.backgroundColor"></el-color-picker>
+                </span>
+                <label>字体颜色</label>
+                <span>
+                  <el-color-picker v-model="selectElement.parames.color"></el-color-picker>
+                </span>
+              </div>
+              <div class="attribute-group">
+                <label>切换方式</label>
+                <span>
+                  <el-select class="attribute-position-select" v-model="selectElement.parames.fontFamily" placeholder="请选择" size="mini">
+                    <el-option label="顺序" value="1"></el-option>
+                    <el-option label="随机" value="2"></el-option>
+                  </el-select>
                 </span>
               </div>
             </div>
-
+            <!--图片/PDF组件参数-->
+            <div class="layout-assembly-attribute" v-if="selectElement.type === 2">
+              <div class="attribute-group">
+                <label>切换动画</label>
+                <span>
+                  <el-select class="attribute-position-select" v-model="selectElement.parames.animation" placeholder="请选择" size="mini">
+                    <el-option label="随机" value="1"></el-option>
+                  </el-select>
+                </span>
+              </div>
+              <div class="attribute-group">
+                <label>切换间隔</label>
+                <span>
+                  <el-input class="attribute-position-time" v-model="selectElement.parames.h" :prefix="'H'" :min="0" :max="23" :controls="false" size="mini"></el-input>
+                  <el-input class="attribute-position-time" v-model="selectElement.parames.m" :prefix="'H'" :min="0" :max="59" :controls="false" size="mini"></el-input>
+                  <el-input class="attribute-position-time" v-model="selectElement.parames.s" :prefix="'H'" :min="0" :max="59" :controls="false" size="mini"></el-input>
+                </span>
+              </div>
+              <div class="attribute-group">
+                <label>切换方式</label>
+                <span>
+                  <el-select class="attribute-position-select" v-model="selectElement.parames.fontFamily" placeholder="请选择" size="mini">
+                    <el-option label="顺序" value="1"></el-option>
+                    <el-option label="随机" value="2"></el-option>
+                  </el-select>
+                </span>
+              </div>
+            </div>
           </el-aside>
         </el-container>
       </el-main>
@@ -155,15 +242,31 @@ export default {
         ]
       },
       selectElement: {
-        name: '组件一',
-        active: true,
-        style: {
+        name: '组件一',                          // 组件名字
+        active: true,                           // 是否被选中
+        type: 2,                                // 组件类型
+        style: {                                // 组件样式
           top: '10px',
           left: '10px',
           width: '200px',
           height: '200px',
           background: this.randomBgColor(),
           'z-index': 0
+        },
+        // parames: {                              // 字幕组件参数
+        //   direction: '1',                       // 方向{1: 从右到左, 2: 从下到上}
+        //   speed: 3,                             // 速度
+        //   fontFamily: '1',                      // 字体
+        //   backgroundColor: '#D8D8D8',
+        //   color: '#D8D8D8',
+        //   switchType: '1'                       // 切换方式{1:顺序,2:随机}
+        // },
+        parames: {                              // 图片组件参数
+          animation: '1',                       // 动画类型{1: 从右到左, 2: 从下到上}
+          h: 3,                                 // 小时
+          m: 3,                                 // 分钟
+          s: 3,                                 // 秒数
+          switchType: '1'                       // 切换方式{1:顺序,2:随机}
         }
       },
       playlistInfoVisible: false
@@ -333,11 +436,21 @@ export default {
   /*组件属性*/
   .layout-assembly-attribute {
     padding: 20px;
+    border-top: 1px solid rgba(0,0,0,0.1);
     font-size:14px;
     color:rgba(0,0,0,0.65);
   }
 
+  .layout-assembly-attribute:nth-of-type(1) {
+    border-top: none;
+  }
+
   .layout-assembly-attribute label {
+    padding-right: 5px;
+  }
+
+  .layout-assembly-attribute span {
+    padding-left: 5px;
     padding-right: 10px;
   }
 
@@ -345,4 +458,25 @@ export default {
     padding-left: 2px;
     padding-right: 2px;
   }
+
+  .attribute-group {
+    margin-bottom: 15px;
+  }
+
+  /*位置*/
+  .attribute-position-input {
+    width: 70px;
+  }
+
+  .attribute-position-select {
+    width: 120px;
+  }
+
+  .attribute-position-time {
+    width: 100px;
+  }
+
+  /*.attribute-position-time .el-input-number.is-controls-right .el-input__inner {*/
+    /*padding: 0;*/
+  /*}*/
 </style>
